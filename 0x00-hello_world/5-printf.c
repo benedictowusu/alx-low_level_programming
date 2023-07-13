b7ef4b2a96f6d0c57b8c7570721c9571bf85eb2e
Benedict Owusu <beneyjoy@gmail.com>
2023-07-13 22:33:20 +0000

    printing with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - printing a line of code with the printf function
+ * Return: 0(Success)
+ */
+int main(void)
+{
+	printf("\"with proper grammar, but the outcome is a piece of art");
+	return (0);
+}
