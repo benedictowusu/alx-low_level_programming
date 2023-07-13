dbdee72dc99307667884a1cdadfd194d00c5ec24
Benedict Owusu <beneyjoy@gmail.com>
2023-07-13 23:09:04 +0000

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
-#include<stdio.h>
-#include<unistd.h>
+#include <stdio.h>
+#include <unistd.h>
 
 /**
- * main - Pirnt a line
- *
- * Return: 1(Success)
+ * main - A C program that prints a line
+ *   
+ * Return: 1 (Success)
  */
 int main(void)
 {
-	write(2,"and that piece of art is useful\n" - Dora Korpar, 2015-10-19\n", 59);
+	write(2,
+			"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
