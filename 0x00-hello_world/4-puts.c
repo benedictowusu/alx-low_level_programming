1ae6934c62ef84a48ee5b884e8d6107dcb473197
Benedict Owusu <beneyjoy@gmail.com>
2023-07-13 22:24:17 +0000

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,8 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void){
-	puts("\"Programming is like a building a multinlingual puzzle");
+int main(void)
+{
+	puts("\"Programming is like a building a multilingual puzzle");
 	return (0);
 }
