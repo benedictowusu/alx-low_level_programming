eb16b7db447ff38993d8ea3c6832ce1d8d5576d1
Benedict Owusu <beneyjoy@gmail.com>
2023-07-15 04:54:58 +0000

    without q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - printing without q and e
+ *
+ * Return: 0(Success)
+ *
+ */
+int main(void)
+{
+	char lower = 'a';
+	for (lower = 'a'; lower <= 'z'; lower++)
+	{
+		if ((lower == 'q') || (lower == 'e'))
+		{
+			continue;
+		}
+		putchar(lower);
+	}
+	putchar('\n');
+	return (0);
+}
