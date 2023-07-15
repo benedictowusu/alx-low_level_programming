3b522bcc520eb850c32a4f7aec1dd33512b9cc0b
Benedict Owusu <beneyjoy@gmail.com>
2023-07-15 05:43:31 +0000

    base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - base 
+ *
+ * Return: 0(Success)
+ *
+ */
+int main(void)
+{
+	int base16;
+
+	for (base16 = 48; base16 < 58;base16++)
+	{
+		putchar(base16);
+	}
+	for (base16 = 97; base16 < 103; base16++)
+	{
+		putchar(base16);
+	}
+	putchar('\n');
+	return (0);
+}
