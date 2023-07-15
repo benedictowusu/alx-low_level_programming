8ccdc47624489456d689b03a10e2e11410b34e33
Benedict Owusu <beneyjoy@gmail.com>
2023-07-15 04:32:58 +0000

    low n upp

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - printing alphabets
+ *
+ * Return: 0(Success)
+ *
+ */
+int main(void)
+{
+	char low = 'a';
+	char upp = 'A';
+
+	for (low = 'a'; low <= 'z'; low++)
+	{
+		putchar(low);
+	}
+	for (upp = 'A'; upp <= 'Z'; upp++)
+	{
+		putchar(upp)
+	}
+	return (0);
