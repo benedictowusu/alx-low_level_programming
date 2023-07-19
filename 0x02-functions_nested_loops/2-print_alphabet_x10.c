c105d8e45e6fa996d1966e3ebc5db52e17522a05
Benedict Owusu <beneyjoy@gmail.com>
2023-07-19 05:59:23 +0000

    alpha

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,15 +8,14 @@ void print_alphabet_x10(void)
 	char alpha = 'a';
 	int print = 1;
 
+	while (print <= 10)
+	{
 	for (alpha = 'a'; alpha <= 'z'; alpha++)
 	{
 		_putchar(alpha);
 	}
 	_putchar('\n');
-
-	while (print <= 10)
-	{
-		print_alphabet_x10();
-		print++;
+	print_alphabet_x10();
+	print++;
 	}
 }
