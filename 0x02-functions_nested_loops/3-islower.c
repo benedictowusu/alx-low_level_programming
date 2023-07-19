af483632c2639bb40b90b0f0928774c74cedfc56
Benedict Owusu <beneyjoy@gmail.com>
2023-07-19 02:35:53 +0000

    js trying

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#inlcude "main.h"
+/**
+ * main - printing lower case
+ *
+ * Return: 0(Success)
+ *
+ */
+int main(void)
+{
+	int c;
+
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	return (0);
+}
