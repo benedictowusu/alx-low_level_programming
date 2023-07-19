33fa4d98d3b92065a74a1aa86e4f1fffc99b7c8e
Benedict Owusu <beneyjoy@gmail.com>
2023-07-19 03:04:52 +0000

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * main - printing alphabets
  *
@@ -12,13 +11,7 @@ void print_alphabet(void)
 
 	for (alpha = 'a'; alpha <= 'z'; alpha++)
 	{
-		putchar(alpha);
+		_putchar(alpha);
 	}
-	putchar('\n');
-}
-int main(void)
-{
-	print_alphabet();
-
-	return (0);
+	_putchar('\n');
 }
--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 #include <unistd.h>
 /**
  * _putchar to send char c to stdout
