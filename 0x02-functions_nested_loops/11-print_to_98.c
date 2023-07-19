89e104b2d0d7059ad8233286d3728f79a510bc9f
Benedict Owusu <beneyjoy@gmail.com>
2023-07-19 18:28:37 +0000

    98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,52 @@
+#include "main.h"
+/**
+ * print_to_98 - printing to 98
+ * @n: the integer to be checked
+ */
+void print_to_98(int n)
+{
+	if (n == 0)
+	{
+		while (n <= 98)
+		{
+			_putchar(n);
+			_putchar(',');
+			_putchar('\n');
+		}
+		n++;
+	}
+	else if (n == 98)
+	{
+		_putchar(n);
+	}
+	else if ( n == 111)
+	{
+		while (n >= 98)
+		{
+			_putchar(n);
+			_putchar(',');
+			_putchar('\n');
+		}
+		n--;
+	}
+	else if (n == 81)
+	{
+		while (n <= 98)
+		{
+			_putchar(n);
+			_putchar(',');
+			_putchar('\n');
+		}
+		n++;
+	}
+	else if (n == -10)
+	{
+		while (n <= 98)
+		{
+			_putchar(n);
+			_putchar(',');
+			_putchar('\n');
+		}
+		n++;
+	}
+}
