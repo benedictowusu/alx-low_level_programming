1f10f66473311b0bd065a519943c698e56bd4ade
Benedict Owusu <beneyjoy@gmail.com>
2023-07-19 19:30:15 +0000

    98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,40 +6,18 @@
  */
 void print_to_98(int n)
 {
-	if (n == 0)
+	while (n < 98)
 	{
-		while (n <= 98)
-		{
-			printf("%i\n, ", n);
-			n++;
-		}
+		printf("%i, ", n);
+		n++;
 	}
-	else if (n == 98)
+	while (n > 98)
 	{
-		printf("%i\n ", n);
+		printf("%i, ", n);
+		n--;
 	}
-	else if ( n == 111)
+	if (n == 98)
 	{
-		while (n >= 98)
-		{
-			printf("%i\n, ", n);
-			n--;
-		}
-	}
-	else if (n == 81)
-	{
-		while (n <= 98)
-		{
-			printf("%i\n, ", n);
-			n++;
-		}
-	}
-	else if (n == -10)
-	{
-		while (n <= 98)
-		{
-			printf("%i\n, ", n);
-			n++;
-		}
+		printf("%i", n);
 	}
 }
