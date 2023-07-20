daa4dddd5890969420038d00ad213f31816d7d15
Benedict Owusu <beneyjoy@gmail.com>
2023-07-20 01:51:27 +0000

    103

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - not exceeding
+ *
+ * Return: 0(Success)
+ */
+int main(void)
+{
+	int count = 1;
+	unsigned long x, y, z, sum;
+
+	x = 1, y = 2, sum = 0;
+	while (count <= 33)
+	{
+		while (x < 4000000 && (x % 2 == 0))
+		{
+			sum += j;
+		}
+		z = x + y;
+		x = y;
+		y = z;
+	count++;
+	}
+	printf("%lu\n", sum);
+	return (0);
+}
