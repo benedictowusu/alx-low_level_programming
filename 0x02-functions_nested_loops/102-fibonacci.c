85f90deaaea33387a0900781851f9de435d519d5
Benedict Owusu <beneyjoy@gmail.com>
2023-07-20 01:35:44 +0000

    102

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>>
+#include "main.h"
+/**
+ * main - printing first fibonacci 
+ *
+ * Return: 0(success)
+ */
+int main(void)
+{
+	int count, prev, nex, sum;
+
+	count = 0, prev = 0, nex = 1;
+	while (count < 50)
+	{
+		sum = prev + nex;
+		printf ("%i", sum);
+
+		prev = nex;
+		nex = sum;
+
+		if (count == 49)
+		{
+			printf("\n");
+		}
+		else
+		{
+			printf(", ");
+		}
+		count++;
+	}
+	return (0);
+}
