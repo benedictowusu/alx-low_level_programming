9eb66b3a44902d2b832fac968ab6c59f2913ccc0
Benedict Owusu <beneyjoy@gmail.com>
2023-07-20 01:20:34 +0000

    101

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,15 +8,15 @@
 int main(void)
 {
 	int n, p;
-	p = 0;
 
+	p = 0;
 	for (n = 0; n < 1024; n++)
 	{
 		if (n % 3 == 0 || n % 5 == 0)
 		{
-			p =+ n;
+			p = + n;
 		}
-		printf("%i\n", p);
 	}
+	printf("%i\n", p);
 	return (0);
 }
