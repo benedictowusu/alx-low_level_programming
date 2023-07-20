7082698b64cda8eb0e3db24e120884e94a5078ca
Benedict Owusu <beneyjoy@gmail.com>
2023-07-20 01:15:12 +0000

    101

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - sum of 3 and 5
+ *
+ * Return: 0(Success)
+ */
+int main(void)
+{
+	int n, p;
+	p = 0;
+
+	for (n = 0; n < 1024; n++)
+	{
+		if (n % 3 == 0 || n % 5 == 0)
+		{
+			p =+ n;
+		}
+		printf("%i\n", p);
+	}
+}
