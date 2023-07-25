c94f681d0d8022d624f88cc746eee1a9d670b331
Benedict Owusu <beneyjoy@gmail.com>
2023-07-25 01:31:26 +0000

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * puts2 - print every other character
+ * @str: string
+ * return: print
+ */
+void puts2(char *str)
+{
+	int len = 0;
+	int x = 0;
+	char *y = str;
+	int z;
+
+	while (*y != '\0')
+	{
+		y++;
+		len++;
+	}
+	x = len - 1;
+	for (z = 0; z <= x; z++)
+	{
+		if (z % 2 == 0)
+			_putchar(str[z]);
+	}
+}
