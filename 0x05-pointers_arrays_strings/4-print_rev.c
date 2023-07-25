0bf7735fa7ef7d987c4aeb8161eaf9673081c8ba
Benedict Owusu <beneyjoy@gmail.com>
2023-07-25 01:15:05 +0000

    print

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_rev - reverse string
+ * @s: string
+ * return: 0
+ */
+void print_rev(char *s)
+{
+	int len = 0;
+	int x;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	s--;
+	for (x = len; x > 0; x--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
