ef478087982e25e4a76042b4530af48a251913e9
Benedict Owusu <beneyjoy@gmail.com>
2023-07-27 19:26:56 -0700

    low

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,20 +1,19 @@
 #include "main.h"
 /**
  * string_toupper - function to change lowercase to upper
- * @ptr: pointer
  * @str: pointer
  *
  * Return: str
  */
 char *string_toupper(char *str)
 {
-	char *ptr = str;
+	char ptr = str;
 
-	while (*ptr != '\0')
+	while (ptr != '\0')
 	{
-		if (*ptr >= 'a' && *ptr <= 'z')
+		if (ptr >= 'a' && ptr <= 'z')
 		{
-			*ptr = *ptr - 'a' + 'A';
+			ptr = ptr - 'a' + 'A';
 		}
 		ptr++;
 	}
