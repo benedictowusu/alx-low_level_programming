6fa374875184d28318970a5be6e5ba06736d7857
Benedict Owusu <beneyjoy@gmail.com>
2023-07-27 19:28:43 -0700

    low

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -7,13 +7,13 @@
  */
 char *string_toupper(char *str)
 {
-	char ptr = str;
+	char *ptr = str;
 
-	while (ptr != '\0')
+	while (*ptr != '\0')
 	{
-		if (ptr >= 'a' && ptr <= 'z')
+		if (*ptr >= 'a' && *ptr <= 'z')
 		{
-			ptr = ptr - 'a' + 'A';
+			*ptr = *ptr - 'a' + 'A';
 		}
 		ptr++;
 	}
