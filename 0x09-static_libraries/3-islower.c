109973ee290b02b0f602efe7ee8097c1e8acac46
Benedict Owusu <beneyjoy@gmail.com>
2023-08-04 15:33:57 +0000

    libmy

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _islower - printing lower case
+ *@c: the char to be checked
+ * Return: 1(lowercase), else 0
+ *
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	return (0);
+}
---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strcpy - copies a string
+ * @dest: copy to
+ * @src: copy from
+ * return: string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int y = 0;
+	int x = 0;
+
+	while (*(src + y) != '\0')
+	{
+		y++;
+	}
+	for (x = 0; x < y; x++)
+	{
+		dest[x] = src[x];
+	}
+	dest[y] = '\0';
+	return (dest);
+}
