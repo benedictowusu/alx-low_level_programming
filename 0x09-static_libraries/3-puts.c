5385d462c4eb662720bb6a66642165d9e1bd4e43
Benedict Owusu <beneyjoy@gmail.com>
2023-08-05 20:56:43 -0700

    static

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -8,6 +8,6 @@ void _puts(char *str);
 */
 int main(void)
 {
-	_puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
